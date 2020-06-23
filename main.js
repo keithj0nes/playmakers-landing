7054138fc2db8f6d4c94bee5aa64a73835064430
keith <keith@raincityambience.com>
2020-06-22 21:07:52 -0400

    add firebase to project

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCFIHw1f_WZWGNCF24KFDhfcUnsfIJAdWs",
+    authDomain: "rn-auth-15547.firebaseapp.com",
+    databaseURL: "https://rn-auth-15547.firebaseio.com",
+    projectId: "rn-auth-15547",
+    storageBucket: "rn-auth-15547.appspot.com",
+    messagingSenderId: "489908155127",
+    appId: "1:489908155127:web:8b347c640c7a7ae4cad9f2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+
 const CHECKBOX_DATA = [
     { name: 'hockey' },
     { name: 'baseball' },
@@ -13,10 +31,12 @@ let selectedSports = [];
 const checkboxField = document.querySelector('.checkbox-field');
 const form = document.querySelector('form');
 const formSuccess = document.querySelector('.form-success');
+const email = document.getElementById('email');
+const invalidEmail = document.querySelector('.invalid-email');
 function createCheckbox(details) {
     return `
     <label class="checkbox-container">${details.name.charAt(0).toUpperCase()}${details.name.slice(1)}
-        <input type="checkbox" id="other" onClick="addSport('${details.name}')">
+        <input type="checkbox" onClick="addSport('${details.name}')">
         <span class="checkmark"></span>
     </label>
     `
@@ -25,7 +45,6 @@ function createCheckbox(details) {
 function addSport(id) {
     const ind = selectedSports.indexOf(id); 
     ind >= 0 ? selectedSports.splice(ind, 1) : selectedSports.push(id)
-    console.log(selectedSports)
 }
 
 CHECKBOX_DATA.forEach(data => {
@@ -33,20 +52,44 @@ CHECKBOX_DATA.forEach(data => {
     checkboxField.insertAdjacentHTML('beforeend', m);
 })
 
+
 function handleSubmit(e) {
     e.preventDefault();
 
+    // check email validation
+    const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(!re.test(email.value)) {
+        // add alert that it's bad bad
+        return invalidEmail.classList.add('show');
+    }
+
     form.style.opacity = 0.5;
-    return new Promise(r => {
-        setTimeout(() => {
-            return r(handleSubmitSuccess());
-        }, 2000);
+    form.style.pointerEvents = 'none';
+    
+    // send to firebase
+    storeToFB().then(rs => {
+        
+        // alert user success - wait 1.5 seconds
+        return new Promise(r => {
+            setTimeout(() => {
+                return r(handleSubmitSuccess());
+            }, 2000);
+        })
     })
 }
 
 
-
 function handleSubmitSuccess() {
-    form.style.display = 'none';
+    form.classList.add('hide');
     formSuccess.style.display = 'block';
+}
+
+function storeToFB() {
+    return firebase.database().ref('/').push().set({
+        date_submitted: Date(),
+        email: email.value,
+        sports : selectedSports
+    }).then(() => {
+        return true
+    })
 }
